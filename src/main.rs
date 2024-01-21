084f88085021145a345b612bb0ae7264852e36b6
FireIsGood <fireisadequate@gmail.com>
2024-01-20 17:24:16 -0800

    Add timing to the output

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,19 @@ use crate::args::get_config;
 use crate::files::iterate_sinners;
 
 fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
     println!("Generating images...");
+    color_eyre::install()?;
+    // Timing function
+    let time_before = std::time::Instant::now();
+
     let config_file = get_config()?;
 
     let sinners_generated = iterate_sinners(&config_file)?;
 
     let image_count_suffix = if sinners_generated == 1 { "" } else { "s" };
-    println!("Generated {sinners_generated} image{image_count_suffix}!");
+    let time_elapsed = time_before.elapsed().as_secs_f32();
+    println!(
+        "Generated {sinners_generated} image{image_count_suffix} in {time_elapsed:.2?} seconds!"
+    );
     Ok(())
 }
